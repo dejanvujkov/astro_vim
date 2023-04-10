2266f42505c1f0d46b47205a802c739d8373a53b
Dejan Vujkov <vujkovdejan@gmail.com>
2023-04-10 17:38:48 +0200

    Added support for C#, rust, TS and docker

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = { "lua_ls", "omnisharp", "cssls", "rust_analyzer", "sqlls", "dockerls",
+        "docker_compose_language_service", "html", "tsserver" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,14 +14,14 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "prettier" },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = { "coreclr" },
     },
   },
 }
